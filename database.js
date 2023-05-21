0b4fa9a82bb9cd5512da14b2721c0ff424f69397
Ozayr <uzairzaviar@gmail.com>
2023-05-21 18:43:47 +0500

    dot env added

--- database.js
+++ database.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
+require('dotenv').config("./env")
 
-const sequelize = new Sequelize('thesis', 'anyroad', 'uzair121212', {
-  host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: 'postgres',
 });
 
